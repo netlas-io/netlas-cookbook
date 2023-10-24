a4ef6b6f3ca9706beafbbf7e76c2e27b98f9697b
Netlas.io <79084675+netlas-io@users.noreply.github.com>
2023-10-24 11:48:38 +0300

    Add files via upload

---
+++ scripts/common_problems/domain_list_search.py
@@ -0,0 +1,23 @@
+import netlas
+
+apikey = "YOUR_API_KEY"
+
+# create new connection to Netlas
+netlas_connection = netlas.Netlas(api_key=apikey)
+
+
+# read file domains.txt line by line
+with open("scripts/common_problems/domains.txt") as f:
+    # save each line to domain variable
+    for domain in f:
+         # retrieve data from responses by query `domain:domainname`
+         netlas_query = netlas_connection.query(query="domain:"+domain,datatype="domain-whois")
+
+
+         # iterate over data and print:  ip, isp
+         for response in netlas_query['items']:
+             print (response['data']['ip'])
+             print (response['data']['isp'])
+        
+         pass
+    pass
