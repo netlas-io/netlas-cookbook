645ae7a6aeabb3ba67605d8f031ef7c607470f70
Netlas.io <79084675+netlas-io@users.noreply.github.com>
2023-10-25 12:13:19 +0300

    Add files via upload

---
+++ scripts/osint/juicyinfo_search.py
@@ -0,0 +1,16 @@
+import netlas
+
+apikey = "YOUR_API_KEY"
+
+# create new connection to Netlas
+netlas_connection = netlas.Netlas(api_key=apikey)
+
+# retrieve data from responses by query `uri:*lidl.* AND http.body:pdf`
+netlas_query = netlas_connection.query(query='uri:*lidl.* AND http.body:pdf')
+
+
+# iterate over data and print: uri, body
+for response in netlas_query['items']: 
+    print (response['data']['uri'])
+    print (response['data']['http']['body'])
+pass
