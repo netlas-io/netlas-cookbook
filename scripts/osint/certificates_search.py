73526dd20a0e735f2e3bd09a66631e85331cb98f
Netlas.io <79084675+netlas-io@users.noreply.github.com>
2023-11-03 17:58:07 +0300

    Add files via upload

---
+++ scripts/osint/certificates_search.py
@@ -0,0 +1,18 @@
+import netlas
+
+apikey = "YOUR_API_KEY"
+
+# create new connection to Netlas
+netlas_connection = netlas.Netlas(api_key=apikey)
+
+# retrieve data from responses by query `certificate.issuer.street_address:*mcgill*`
+netlas_query = netlas_connection.query(query="certificate.issuer.street_address:*mcgill*")
+
+
+print (type(netlas_query))
+
+# iterate over data and print: ip, url, cetificate issuer 
+for response in netlas_query['items']:
+    print (response['data']['uri'])
+    print (response['data']['certificate']['issuer'])  
+pass
